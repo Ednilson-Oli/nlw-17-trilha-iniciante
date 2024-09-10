2f6e773dcc23c7862e0e469fc9c5157909ccb5e4
Ednilson-Oli <ednilsonoliveira2@hotmail.com>
2024-09-09 21:35:39 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-// hello world
+//arrays, objetos
+let meta = {
+    value:  "Ler um livro todo mês",
+    checked: false,
+    log:(info)=>{
+        console.log(info)
+    }
+}
+meta.value = "Não é mais um livro"
+meta.log(meta.value)
 
-let mensagem ="Olá mundo"
-console.log(mensagem);
-console.log(mensagem);
-console.log(mensagem);
+//function
+const criarMeta = ()=>{
+
+}
+
+// function criarMeta(){}
